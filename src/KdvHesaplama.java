d82f3300c864a1e1e32e73075c1e226e3286de0a
Berke Göze <gozeberke@gmail.com>
2021-12-20 16:46:53 +0300

    Update KdvHesaplama.java

--- src/KdvHesaplama.java
+++ src/KdvHesaplama.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
 public class KdvHesaplama {
+    public static void main(String[] args) {
+        double KDV=0.18,urun_fiyat,kdv_fiyat,kdv_tutari;
+        System.out.print("Ürünün fiyatını giriniz: ");
+        Scanner fiyat=new Scanner(System.in);
+        urun_fiyat=fiyat.nextDouble();
+        kdv_fiyat= urun_fiyat+urun_fiyat*KDV;
+        kdv_tutari=urun_fiyat*KDV;
+        System.out.println("Ürünün KDV'li fiyatı: "+kdv_fiyat+"\nÜrün fiyatı: "+urun_fiyat+"\nKDV tutarı: "+kdv_tutari);
+    }
 }
