2b0f4a26dc3d5fcf1a8c4a85280eb39f70f86aef
gozeberke <73718014+gozeberke@users.noreply.github.com>
2021-12-20 17:26:36 +0300

    Java_ornek

--- src/KdvHesaplama.java
+++ src/KdvHesaplama.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 public class KdvHesaplama {
     public static void main(String[] args) {
-        double KDV=0.18,urun_fiyat,kdv_fiyat,kdv_tutari;
+        double KDV,urun_fiyat,kdv_fiyat,kdv_tutari;
         System.out.print("Ürünün fiyatını giriniz: ");
         Scanner fiyat=new Scanner(System.in);
         urun_fiyat=fiyat.nextDouble();
+        if(urun_fiyat>0 && urun_fiyat<1000){
+            KDV=0.18;
+        }else{
+            KDV=0.08;
+        }
         kdv_fiyat= urun_fiyat+urun_fiyat*KDV;
         kdv_tutari=urun_fiyat*KDV;
         System.out.println("Ürünün KDV'li fiyatı: "+kdv_fiyat+"\nÜrün fiyatı: "+urun_fiyat+"\nKDV tutarı: "+kdv_tutari);
+
+
     }
 }
